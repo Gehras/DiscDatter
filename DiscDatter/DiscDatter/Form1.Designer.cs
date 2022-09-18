8b1819e91e7d1f59b893c9b6b184524133300440
Geras <chancedavies@mycwi.cc>
2022-09-18 16:12:05 -0600

    First Peer Programming Run
    Major changes:
    1. Deletion of original program
    2. Recreation
    3. Parsing of txt file to collect game and roms
    4. Modification of xml to add game and roms

---
+++ DiscDatter/DiscDatter/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DiscDatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submissionButton = new System.Windows.Forms.Button();
+            this.datButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtPathLabel = new System.Windows.Forms.Label();
+            this.datPathLabel = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // submissionButton
+            // 
+            this.submissionButton.Location = new System.Drawing.Point(39, 61);
+            this.submissionButton.Name = "submissionButton";
+            this.submissionButton.Size = new System.Drawing.Size(134, 52);
+            this.submissionButton.TabIndex = 0;
+            this.submissionButton.Text = "Browse";
+            this.submissionButton.UseVisualStyleBackColor = true;
+            this.submissionButton.Click += new System.EventHandler(this.submissionButton_Click);
+            // 
+            // datButton
+            // 
+            this.datButton.Location = new System.Drawing.Point(295, 61);
+            this.datButton.Name = "datButton";
+            this.datButton.Size = new System.Drawing.Size(134, 52);
+            this.datButton.TabIndex = 1;
+            this.datButton.Text = "Browse";
+            this.datButton.UseVisualStyleBackColor = true;
+            this.datButton.Click += new System.EventHandler(this.datButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Specify !submissionInfo.txt:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Specify .dat";
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(39, 265);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(390, 37);
+            this.generateButton.TabIndex = 4;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "All Dump Files|*.txt;*.dat";
+            // 
+            // txtPathLabel
+            // 
+            this.txtPathLabel.Location = new System.Drawing.Point(35, 131);
+            this.txtPathLabel.Name = "txtPathLabel";
+            this.txtPathLabel.Size = new System.Drawing.Size(394, 54);
+            this.txtPathLabel.TabIndex = 5;
+            this.txtPathLabel.Text = "!submissionInfo.txt location: ";
+            // 
+            // datPathLabel
+            // 
+            this.datPathLabel.Location = new System.Drawing.Point(35, 200);
+            this.datPathLabel.Name = "datPathLabel";
+            this.datPathLabel.Size = new System.Drawing.Size(394, 62);
+            this.datPathLabel.TabIndex = 6;
+            this.datPathLabel.Text = ".dat location:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 320);
+            this.Controls.Add(this.datPathLabel);
+            this.Controls.Add(this.txtPathLabel);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.datButton);
+            this.Controls.Add(this.submissionButton);
+            this.Name = "Form1";
+            this.Text = "DiscDatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submissionButton;
+        private System.Windows.Forms.Button datButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label txtPathLabel;
+        private System.Windows.Forms.Label datPathLabel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ DiscDatter/DiscDatter/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DiscDatter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string datFile = "";
+        string txtFile = "";
+        private void submissionButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog.FileName.EndsWith("txt") == true)
+                {
+                    txtPathLabel.Text = openFileDialog.FileName.ToString();
+                    txtFile = openFileDialog.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("Please select a valid txt file");
+                }
+            }
+        }
+
+        private void datButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog.FileName.EndsWith("dat") == true)
+                {
+                    datPathLabel.Text = openFileDialog.FileName.ToString();
+                    datFile = openFileDialog.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("Please select a valid dat file");
+                }
+            }
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            var parsedName = "";
+            List<string> romLines = new List<string>();
+            StreamReader txtReader = new StreamReader(txtFile);
+            StreamReader datReader = new StreamReader(datFile);
+            //find line with "rom name" header
+            //copy line from header up to cuesheet
+            //create new game name using rom name
+            //add rom name info to this game name
+            bool nameFound = false;
+            StreamReader inputfile = File.OpenText(txtFile);
+            using (txtReader)
+            {
+                var line = "";
+                while ((line = txtReader.ReadLine()) != null)
+                {
+                    //Rom line-finding section
+                    if (line.Contains("<rom name=\""))
+                    {
+                        romLines.Add("\t\t" + line);
+                    }
+
+                    //Name-finding section
+                    if (nameFound == false && line.Contains("<rom name=\""))
+                    {
+                        nameFound = true;
+                        var parseLine = line.Split(new string[] { "<rom name=\"" }, StringSplitOptions.None);
+                        if(parseLine.Length >= 2)
+                        {
+                            var nameToParse = parseLine[1].Split(new string[] { " (Track" }, StringSplitOptions.None);
+                            parsedName = nameToParse[0];
+                            //debug for NAME
+                            //MessageBox.Show(parsedName);
+                        }
+                    }
+                }
+                //debug for ROM lines
+                //for(int i=0;i<romLines.Count;i++)
+                //{
+                  //  MessageBox.Show(romLines[i]);
+                //}
+            }
+            //Close readers
+            datReader.Close();
+            txtReader.Close();
+            //insert information into dat
+            var datLines = File.ReadAllLines(datFile);
+            //Remove last </datafile> line
+            File.WriteAllLines(datFile, datLines.Take(datLines.Length - 1).ToArray());
+            //Add game name line
+            var gameName = "\t<game name=\"" + parsedName + "\">";
+            File.AppendAllText(datFile, gameName + Environment.NewLine);
+            //Add description line
+            var descName = "\t\t<description>" + parsedName + "</description>";
+            File.AppendAllText(datFile, descName + Environment.NewLine);
+            //Add rom name lines 
+            romLines.ForEach(item => File.AppendAllText(datFile, item + Environment.NewLine));
+            //Add game terminator
+            File.AppendAllText(datFile, "\t</game>" + Environment.NewLine);
+            //Add datafile terminator
+            File.AppendAllText(datFile, "</datafile>" + Environment.NewLine);
+            //Show message saying complete
+            MessageBox.Show("File modified!");
+        }
+    }
+}
